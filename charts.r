4aee09c3aa2fda200c1f44a8c4a4b3b8e734d2f7
Matheus Tregnago <maugustotreg@gmail.com>
2026-06-01 19:54:21 -0300

    feat: graficos para todas as fases

--- charts.r
+++ charts.r
@@ -15,33 +15,30 @@ summary_df <- df |>
   )
 
 
-ggplot(summary_df, aes(x = omp_num_threads, y = speedup, color = algorithm)) +
+long_df <- summary_df |>
+  pivot_longer(
+    cols = c(speedup, eff),
+    names_to = "metric",
+    values_to = "value"
+  ) |>
+  mutate(metric = factor(metric,
+    levels = c("speedup", "eff"),
+    labels = c("Speedup", "Eficiência Paralela (%)")
+  ))
+
+ggplot(long_df, aes(x = omp_num_threads, y = value, color = algorithm)) +
   geom_point() +
   geom_line() +
-  scale_y_continuous(breaks = seq(0, 20, by = 5), limits = c(0, 20)) +
+  facet_wrap(~metric, scales = "free_y") +
   labs(
     x = "Numero de Threads",
-    y = "Speedup",
-    title = "Speedup pelo número de threads",
+    y = NULL,
+    title = "Speedup e Eficiência Paralela pelo número de threads",
     color = "Algoritmos"
   ) +
   theme_bw()
 
-ggsave("plots/speedup.pdf", width = 6, height = 4)
-
-
-ggplot(summary_df, aes(x = omp_num_threads, y = eff, color = algorithm)) +
-  geom_point() +
-  geom_line() +
-  labs(
-    x = "Numero de Threads",
-    y = "Eficiência Paralela",
-    title = "Eficiência Paralela pelo número de threads",
-    color = "Algoritmos"
-  ) +
-  theme_bw()
-
-ggsave("plots/eff.pdf", width = 6, height = 4)
+ggsave("plots/speedup_eff.pdf", width = 10, height = 4)
 
 
 df <- read_csv("results/phase2.csv")
@@ -68,3 +65,48 @@ ggplot(summary_df, aes(fill = omp_schedule, y = mean_time, x = factor(chunk))) +
 
 
 ggsave("plots/schedule.pdf", width = 6, height = 4)
+
+df <- read_csv("results/phase3.csv")
+
+summary_df <- df |>
+  group_by(algorithm, omp_proc_bind, omp_places) |>
+  summarise(
+    mean_time = mean(time)
+  )
+
+ggplot(summary_df, aes(x = omp_places, y = omp_proc_bind, fill = mean_time)) +
+  facet_wrap(~algorithm) +
+  geom_tile(color = "white") +
+  geom_text(aes(label = round(mean_time, 2)), color = "black", size = 4) +
+  scale_fill_viridis_c() +
+  theme_minimal()
+ggsave("plots/bind_and_places.pdf", width = 6, height = 4)
+
+
+df <- read_csv("results/phase4.csv")
+
+
+summary_df <- df |>
+  group_by(algorithm, first_touch, omp_num_threads) |>
+  summarise(
+    mean_time = mean(time),
+    sd = sd(time)
+  )
+
+ggplot(summary_df, aes(
+  fill = factor(first_touch), y = mean_time,
+  x = factor(omp_num_threads)
+)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_errorbar(aes(ymin = mean_time - sd, ymax = mean_time + sd),
+    width = .5,
+    position = position_dodge(.9)
+  ) +
+  labs(
+    title = "Stencil2d",
+    fill = "First touch"
+  ) +
+  theme_bw()
+
+
+ggsave("plots/first_touch.pdf", width = 6, height = 4)
